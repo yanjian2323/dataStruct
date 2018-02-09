6c9b4ae2ce6f40860bd245a2fbc04a4bf64729f0
yanjian <yanjian@yunniao.me>
2018-02-09 18:25:01 +0800

    选择排序的基本实现

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import fn from './selectionSort/selectionSort';
+
+let arr = [99,43,12,42,129,43,23,41,1,2,5,6,9,5];
+
+fn(arr);
---
+++ src/selectionSort/selectionSort.js
@@ -0,0 +1,20 @@
+// 选择排序
+const selectionSort = (arr) => {
+	let len = arr.length;
+	let minIndex;
+	for (let i = 0; i < len; i++) {
+		minIndex = i;
+		for (let j = i + 1; j < len; j++) {
+			if (arr[j] < arr[minIndex]) {
+				minIndex = j;
+			}
+		}
+		// 交换位置
+		[arr[minIndex],arr[i]] = [arr[i],arr[minIndex]];
+	}
+	for (let i = 0; i < len; i++) {
+		console.log(...arr);
+	}
+};
+
+export default selectionSort;
