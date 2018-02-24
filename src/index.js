07b7ea1f60d5fd25e4052cba81897bd6974a435f
yanjian <yanjian@yunniao.me>
2018-02-24 19:20:24 +0800

    快速排序，双路快速排序

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import insertionSort2 from './insertionSort/insertionSort2';
 import bubbleSort from './bubbleSort/bubbleSort';
 import bubbleSort2 from './bubbleSort/bubbleSort2';
 import mergeSort from './mergeSort/mergeSort';
+import quickSort from './quickSort/quickSort';
+import quickSort2 from './quickSort/quickSort2';
 
-let n = 100000;
+let n = 1000000;
 let arr = generateRandomArray(n, 1, n);
-// let arr = generateNearlyOrderedArray(n, 10);
+// let arr = generateNearlyOrderedArray(n, 1000);
 let arr2 = copyArray(arr);
 
 // testSort('selectionSort', selectionSort, arr);
-testSort('insertionSort', insertionSort, arr);
+// testSort('insertionSort', insertionSort, arr);
 // testSort('insertionSort2', insertionSort2, arr2);
 // testSort('bubbleSort', bubbleSort, arr);
 // testSort('bubbleSort2', bubbleSort2, arr2);
-testSort('mergeSort', mergeSort, arr2);
-// printArray(arr2);
+// testSort('mergeSort', mergeSort, arr);
+testSort('quickSort', quickSort, arr);
+testSort('quickSort2', quickSort2, arr2);
+// printArray(arr);
---
+++ src/quickSort/quickSort.js
@@ -0,0 +1,27 @@
+function partition (arr, l, r) {
+	let v = arr[l];
+	let j = l;
+	for (let i = l; i <= r; i++) {
+		if (arr[i] < v) {
+			[arr[i], arr[j + 1]] = [arr[j + 1], arr[i]];
+			j++;
+		}
+	}
+	[arr[l],arr[j]] = [arr[j], arr[l]];
+	return j;
+}
+
+function _quickSort (arr, l, r) {
+	if (l >= r) {
+		return;
+	}
+	let p = partition(arr, l, r);
+	_quickSort(arr, l, p - 1);
+	_quickSort(arr, p + 1, r);
+}
+
+function quickSort (arr) {
+	_quickSort(arr, 0, arr.length - 1);
+}
+
+export default quickSort;
